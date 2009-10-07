8fa0a02fd601575ec93e14622449ca2cd056cf32
Pedro Melo <melo@simplicidade.org>
2009-10-07 12:55:11 +0100

    Tweak documentation, added proper credits
    Signed-off-by: Pedro Melo <melo@simplicidade.org>

--- Dist-Zilla-Plugin-MakeMaker-SkipInstall/lib/Dist/Zilla/Plugin/MakeMaker/SkipInstall.pm
+++ Dist-Zilla-Plugin-MakeMaker-SkipInstall/lib/Dist/Zilla/Plugin/MakeMaker/SkipInstall.pm
@@ -51,17 +51,55 @@ In your C<dist.ini> file:
 
 =head1 DESCRIPTION
 
-This small plugin will edit the C<Makefile.PL> file, and override the install target to
-become a no-op.
+This small plugin will edit the C<Makefile.PL> file, and override the
+install target to become a no-op.
 
-This will make your module fail the install phase. It will be built, and tested but will never be installed.
+This will make your module fail the install phase. It will be built, and
+tested but will never be installed.
 
-The most common use for this techinique is for L<Task> modules. Without a proper install phase, you can install your Task module repetedly.
+The most common use for this techinique is for L<Task> modules. Without
+a proper install phase, you can install your Task module repetedly.
+
 
 =head1 CREDITS
 
-The technique was described by ... 
+The technique was described by Marcel Gruenauer (hanekomu) in his
+article "Repeatedly installing Task::* distributions":
+
+L<http://hanekomu.at/blog/dev/20091005-1227-repeatedly_installing_task_distributions.html>
 
 The author just wrapped the concept into a L<Dist::Zilla> plugin.
+
+
+=head1 SEE ALSO
+
+L<Dist::Zilla>.
+
+
+=head1 AUTHOR
+
+Pedro Melo, C<< <melo at cpan.org> >>
+
+
+=head1 COPYRIGHT & LICENSE
+
+Copyright 2009 Pedro Melo.
+
+This program is free software; you can redistribute it and/or modify it
+under the same terms as Perl itself.
+
+
+=begin make-pod-coverage-happy
+
+=over 4
+
+=item after_build()
+
+Edits the C<Makefile.PL> in place, searches for C<WriteMakefile> and
+prepends our override.
+
+=back
+
+=end make-pod-coverage-happy
 
 =cut
